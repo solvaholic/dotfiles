5eb7413b9d434c829b163f75d229f7d8ce380274
Roger D. Winans <solvaholic@github.com>
2017-11-19 20:49:10 -0500

    linuxmint VM has no proc/sys/hw

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -54,12 +54,15 @@ directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-battery_status() {
-  if [[ $(sysctl -n hw.model) == *"Book"* ]]
-  then
-    $ZSH/bin/battery-status
-  fi
-}
+# TODO: Fix or remove battery_status
+#   On linuxmint VM there's no /proc/sys/hw/model
+
+# battery_status() {
+#   if [[ $(sysctl -n hw.model) == *"Book"* ]]
+#   then
+#     $ZSH/bin/battery-status
+#   fi
+# }
 
 export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
