e8647e0a1d96f6a2cd2f934fcedd06312147b843
Roger D. Winans <solvaholic@github.com>
2017-11-23 08:20:40 -0500

    Stub install.sh for awscli

---
+++ aws.symlink/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# @solvaholic's dotfiles - Installer for awscli
+#
+# Use the Brewfile and bootstrap to install awscli.
+#
+# Use this script to ...
+# - Run aws configure (or whatever).
+# - Stub/stage/link configs, templates, ???.
+# - Inventory visible stuff, prompt to set some defaults.
+#   (For example default availability zone or SSH key, default settings
+#   for new instances/buckets/whatever).
+# - (?) Inventory visible stuff, build data for prompts and completions.
+# - Install awscli bash/zsh completions.
+
+exit 0
