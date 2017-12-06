7887ad7231b118f26a9ed50384ce2ecec355343e
solvaholic <solvaholic@users.noreply.github.com>
2017-12-05 22:31:37 -0500

    Make os/install.sh smarter and add MacOS option

--- os/install.sh
+++ os/install.sh
@@ -3,17 +3,27 @@
 # OS
 #
 # These perform OS-specific install and configure tasks.
+#
 
 # TODO: Make this actually work.
 
-# Figure out which package manager we have, and call its script.
+# Figure out which OS and package manager we have, and call its script.
 
-if test $(which apt)
-then
-  # We think we're running a Debian-based Linux, like Linux Mint.
-  sh "$(dirname $0)/apt-install.sh"
-elif test $(which yum)
-then
-  # We think we're running a RedHat-based Linux, like CentOS.
-  sh "$(dirname $0)/yum-install.sh"
-fi
+MYOS="$( sh "$(dirname $0)/../bin/whichos" )"
+
+case $MYOS in
+  Linux)
+    if test $(which apt)
+    then
+      # We think we're running a Debian-based Linux, like Linux Mint.
+      sh "$(dirname $0)/apt-install.sh"
+    elif test $(which yum)
+    then
+      # We think we're running a RedHat-based Linux, like CentOS.
+      sh "$(dirname $0)/yum-install.sh"
+    fi
+    ;;
+  MacOS)
+    sh "$(dirname $0)/mac-install.sh"
+    ;;
+esac
