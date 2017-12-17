a14a44611ab0bf7bf15cffc8a70971f10aede401
solvaholic <solvaholic@noreply.users.github.com>
2017-12-16 21:46:47 -0500

    Add fix_scroll.sh to enable natural scrolling

---
+++ bin/fix_scroll.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+synclient VertScrollDelta=-111
+synclient HorizScrollDelta=-111
+
+# TODO: Fix this to get current value from synclient rather than
+#       arbitrarily set to -111
