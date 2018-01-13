3005d8c46d5db1b88dc96386cc59afc3a1c5913d
Roger D. Winans <solvaholic@users.noreply.github.com>
2018-01-13 10:23:15 -0500

    Make pkg_add less verbose in obsd-install.sh

--- os/obsd-install.sh
+++ os/obsd-install.sh
@@ -7,8 +7,8 @@ set -e
 # Update installed packages and check for available patches.
 # Thank you @nixcraft! https://www.cyberciti.biz/faq/howto-apply-updates-on-openbsd-operating-system/
 # TODO: Make sure /etc/installurl contains a valid path, handle when it doesn't.
-echo "> doas pkg_add -Uuv"
-doas pkg_add -Uuv
+echo "> doas pkg_add -Uu"
+doas pkg_add -Uu
 echo " "
 # TODO: If patches are available then explain how to install them.
 echo "> doas syspatch -c"
