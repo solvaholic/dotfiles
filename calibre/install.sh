00917bbfc4da47251e278dd3b9b038903c460745
Roger D. Winans <solvaholic@users.noreply.github.com>
2019-05-05 09:24:58 -0400

    Delete a few more topics I think I won't need

--- calibre/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Do we have wget? and can we sudo?
-
-# Download and install or update Calibre.
-sudo chown "$( whoami )" /opt && sudo -k chmod 775 /opt
-wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sh
-mkdir -p "$HOME/bin"
-ln -s /opt/calibre/calibre "$HOME/bin"
