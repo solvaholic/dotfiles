d0d33b90912e83677cb4e23ded471d7966837aeb
Roger D. Winans <solvaholic@users.noreply.github.com>
2020-03-29 21:01:56 -0400

    Update atom.symlink/install.sh
    per recommendations from shellcheck

--- atom.symlink/install.sh
+++ atom.symlink/install.sh
@@ -2,53 +2,90 @@
 #
 # Atom installer
 #
-# Install or upgrade Atom (https://atom.io)
+# - atom.symlink/install.sh: Install or upgrade Atom (https://atom.io).
+#
+#     - If Atom is already installed, which version?
+#     - Download the latest release of Atom.
+#     - If the download is newer than the installed version, upgrade.
+#
+# Usage:
+#   ./atom.symlink/install.sh
 #
 
-set -e
 
-# Currently only works for dpkg-based Linux distributions!
-if [ ! -n "$( which dpkg )" ]; then
-  echo "STOP: This currently only works for dpkg-based Linux distributions."
+# Load ~/.dotfiles.conf and $DOTFILES_ROOT/dotfiles.sh.
+# shellcheck disable=SC1090
+{
+  . ~/.dotfiles.conf || exit
+  . "$DOTFILES_ROOT/dotfiles.sh" || exit
+} \
+&& debug "$0: Loaded ~/.dotfiles.conf and ran $DOTFILES_ROOT/dotfiles.sh OK." \
+|| exit
+
+
+# Currently only works for dpkg-based Linux distributions.
+if [ -z "$( command -v dpkg )" ]; then
+  fail "$): This currently only works for dpkg-based Linux distributions."
   exit 1
 fi
+
 
 # TODO: Wrap these in a whichos and stub other package managers' methods.
 # TODO: Find an effective way to test the new/installed versions.
 
-echo "Downloading the newest release of Atom..."
-# Remove any leftover /tmp/atom.deb and download the newest release.
+
+# If Atom is already installed, which version?
+if [ -x "$( command -v atom )" ]; then
+  # Looks like Atom is installed.
+  _curver=$(atom -v | sed -n '/^Atom/ s/^.*: \(.*\)$/\1/p') \
+  && debug "$0: Atom $_curver is currently installed." \
+  || exit
+else
+  # Looks like Atom is not installed.
+  _curver=$(atom -v | sed -n '/^Atom/ s/^.*: \(.*\)$/\1/p') \
+  && debug "$0: Atom $_curver is currently installed." \
+  _curver=""
+fi
+
+
+# Download the latest release of Atom.
+debug "$0: Downloading the latest release of Atom..."
+# Remove any leftover /tmp/atom.deb and download the latest release.
 rm -f /tmp/atom.deb
 curl -sLo /tmp/atom.deb https://atom.io/download/deb
 
-if [ -n "$( dpkg -I /tmp/atom.deb | egrep -o 'Version: .+$' )" ]; then
+
+# If the download is newer than the installed version, upgrade.
+_newver=""
+_newver="$( dpkg -I /tmp/atom.deb | sed -n '/Version/ s/^.*: \(.*\)$/\1/p' )" \
+|| exit
+
+if [ -n "$_newver" ]; then
   # Assume the downloaded .deb is valid, grab its version and continue.
-  NEWVER="$( dpkg -I /tmp/atom.deb | egrep -o 'Version: .+$' )"
-  echo "Atom $NEWVER is the newest release."
+  debug "$0: Downloaded Atom $_newver."
 else
   # Assume the downloaded .deb is not valid, and exit.
-  echo "STOP: Unable to determine version of /tmp/atom.deb!"
+  fail "$0: Unable to determine version of /tmp/atom.deb."
   exit 1
 fi
 
-if [ -n "$( dpkg -s atom | egrep -o 'Version: .+$' )" ]; then
-  # Atom is currently installed, so grab its version and continue.
-  CURVER="$( dpkg -s atom | egrep -o 'Version: .+$' )"
-  echo "Atom $CURVER is currently installed."
-fi
+info "$0: The latest release of Atom is $_newver."
+info "$0: Looks like you have Atom $_curver installed."
 
-# TODO: Is it possible to reach this point with bad NEWVER or CURVER?
-#       And find a more robust way to compare versions. This failed.
 
-if [ "$NEWVER" > "$CURVER" ]; then
-  # NEWVER is greater than CURVER, so uninstall old and install new.
-  echo "Uninstalling Atom $CURVER..."
-  echo "> sudo dpkg -r atom"
+# TODO: Find a more robust way to compare versions. This failed.
+
+_winner="$( (echo "$_curver"; echo "$_newver") | sort -nr - | head -n 1 )"
+
+if [ "$_newver" = "$_winner" ] && [ "$_newver" != "$_curver" ]; then
+  # _newver is greater than _curver, so uninstall old and install new.
+  info "Uninstalling Atom $_curver..."
+  info "> sudo dpkg -r atom"
   sudo dpkg -r atom
-  echo "Installing Atom $NEWVER..."
-  echo "> sudo dpkg -i /tmp/atom.deb"
+  info "Installing Atom $_newver..."
+  info "> sudo dpkg -i /tmp/atom.deb"
   sudo dpkg -i /tmp/atom.deb
 else
-  # NEWVER is less than or equal to CURVER, so pass.
-  echo "It looks like you're already running the latest. Good job!"
+  # _newver is less than or equal to _curver, so pass.
+  ifno "$0: Looks like you're already running the latest. Good job!"
 fi
