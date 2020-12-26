2f27b848b2ce80ebef819a345a764253dcbc04ba
Roger D. Winans <solvaholic@users.noreply.github.com>
2020-12-25 22:23:02 -0500

    Improve link_file() and move check_preq()
    to dotfiles.sh.
    
    The link_file() changes now handle relative $source paths
    and offer the user a smaller set of options when $target already exists.

--- dotfiles.sh
+++ dotfiles.sh
@@ -12,6 +12,8 @@
 # typically ~/.dotfiles.
 #
 
+# shellcheck disable=SC2039
+
 # Define some output functions.
 
 info () {
@@ -35,6 +37,35 @@ debug () {
   if [ -n "$DOTFILES_DEBUG" ] && [ "$DOTFILES_DEBUG" -ne 0 ]; then
     printf "\r  [ \033[0;33m==\033[0m ] %s\n" "$1" >&2
   fi
+}
+
+#
+# Define check_preq() function for ensuring prerequisites are met.
+#
+check_preq () {
+  _self="${_self:-:}"
+  _result=0
+  info "$_self: Check for prerequisites."
+  # COMMANDS
+  for _cmd in id docker; do
+    if command -v "$_cmd" >/dev/null; then
+      debug "$_self: check_preq() found command '$_cmd' OK."
+    else
+      fail "$_self: check_preq() did not find command '$_cmd'."
+      _result=$_result+1
+    fi
+  done
+  # PATH ELEMENTS
+  # shellcheck disable=SC2066
+  for _path in "$HOME/Local/bin"; do
+    if [[ ":$PATH:" = *":$_path:"* ]] ; then
+      debug "$_self: check_preq() found '$_path' in \$PATH OK."
+    else
+      fail "$_self: check_preq() did not find '$_path' in \$PATH."
+      _result=$_result+1
+    fi
+  done
+  return $((_result))
 }
 
 #
@@ -74,7 +105,8 @@ run_as_root () {
   else
     if can_i_sudo; then
       debug "$0: We can sudo, so 'sudo bash -c \"\$_mycmd\"'."
-      user "Attempting to run this command. You may be asked for your password:"
+      user "Attempting to run the command below;"
+      user "You may be asked for your password:"
       info "  sudo bash -c \"$_mycmd\""
       sudo bash -c "$_mycmd"
       return
@@ -93,86 +125,85 @@ link_file () {
   # $source. If $target already exists but is _not_ a link to $source
   # then ask the user what to do.
 
-  source="$1" target="$2" shortsource="${source#$HOME/}"
+  source="$1" target="$2"
   overwrite='' backup='' skip='' action=''
   script_name="link_file()"
 
-  # Do $source and $target look like valid paths? If not then return 1.
-  if [ ! -e "$source" ]; then
-    fail "$script_name: \$source '$source' doesn't exist!"
-    return 1
-  elif [ ! -d "$( dirname "$target" )" ]; then
+  # Do $source and $target look like valid paths?
+  if [ ! -d "$( dirname "$target" )" ]; then
+    # target path looks wrong
     fail "$script_name: \$target '$target' path is invalid!"
+    return 1
+  elif [ "${source:0:1}" = "/" ]; then
+    debug "$script_name: \$source '$source' is an absolute path."
+    if [ ! -e "$source" ]; then
+      fail "$script_name: \$source '$source' doesn't exist!"
+      return 1
+    fi
+  elif [ -n "$source" ]; then
+    debug "$script_name: \$source '$source' is a relative path."
+    pushd "$( dirname "$target" )" >/dev/null || return 1
+    if [ ! -e "$source" ]; then
+      fail "$script_name: \$source '$source' doesn't exist!"
+      return 1
+    fi
+    popd >/dev/null || return 1
+  else
+    # Something is wrong.
+    fail "$script_name: Shit broke yo."
     return 1
   fi
 
-  # If the target already exists, then prompt the user for action.
+  # If $target already exists then prompt the user for action.
   if [ -e "$target" ]; then
 
-    # link_file() inherits overwrite_all, backup_all, and skip_all
-    # from the caller.
+    if [ -L "$target" ]; then
+      # $target is a link. What's it point to?
+      currentSrc="$(readlink "$target")"
+      debug "$script_name: '$target' is a symlink to '$currentSrc'."
+    else
+      currentSrc=''
+    fi
 
-    if [ "$overwrite_all" = "false" ] && [ "$backup_all" = "false" ] && [ "$skip_all" = "false" ]
-    then
+    # If $target is already a link to $source then assume "[s]kip".
+    if [ -n "$currentSrc" ] && [ "$currentSrc" = "$source" ]; then
+      debug "$script_name: '$currentSrc' matches \$source."
+      skip="true"
+    else
 
-      if [ -h "$target" ]; then
-        # Target is a link. What's it point to?
-        currentSrc="$(readlink "$target")"
-      else
-        currentSrc=''
-      fi
+      user "File already exists: '$target'"
+      user "  --> '$source'"
+      user "What do you want to do?"
 
-      # If $target is already a link to $shortsource then then assume "[s]kip".
-      if [ -n "$currentSrc" ] && [ "$currentSrc" = "$shortsource" ]; then
-        skip="true";
-      else
+      # TODO: Validate input here.
 
-        user "File already exists: $target ($(basename "$source"))"
-        user "What do you want to do?"
+      user "[s]kip, [o]verwrite?"
+      read -r action
 
-        # TODO: Validate input here.
-
-        user "[s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
-        read -r action
-
-        case "$action" in
-          o )
-            overwrite="true";;
-          O )
-            overwrite_all="true";;
-          b )
-            backup="true";;
-          B )
-            backup_all="true";;
-          s )
-            skip="true";;
-          S )
-            skip_all="true";;
-          * )
-            ;;
-        esac
-
-      fi
+      case "$action" in
+        o )
+          overwrite="true";;
+        s )
+          skip="true";;
+        * )
+          ;;
+      esac
 
     fi
 
-    overwrite=${overwrite:-$overwrite_all}
-    backup=${backup:-$backup_all}
-    skip=${skip:-$skip_all}
+  fi
 
-    if [ "$overwrite" = "true" ]; then
-      rm -rf "$target"
-      success "$script_name: Removed '$target'."
-    elif [ "$backup" = "true" ]; then
-      mv "$target" "${target}.backup"
-      success "$script_name: Moved '$target' to '${target}.backup'."
-    elif [ "$skip" = "true" ]; then
-      success "$script_name: '$target' is already installed."
-    fi
+  # TODO: Does the script need these 3 lines? :point_down:
+  overwrite=${overwrite:-$overwrite_all}
+  backup=${backup:-$backup_all}
+  skip=${skip:-$skip_all}
+
+  if [ "$skip" = "true" ]; then
+    success "$script_name: Link '$target' is present."
   fi
 
   if [ "$skip" != "true" ]; then
-    ln -s "$shortsource" "$target"
-    success "$script_name: Linked '$source' to '$target'."
+    ln -f -s "$source" "$target"
+    success "$script_name: Linked '$target' --> '$source'."
   fi
 }
